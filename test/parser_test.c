4d8f1b48dfe97a32a59034dc61bbc94713ea2d13
Vikash Kesarwani <vikash.ckiiita30@gmail.com>
2017-12-27 20:50:48 +0530

    add few basic test cases for json_parser

--- test/parser_test.c
+++ test/parser_test.c
@@ -1,23 +1,65 @@
 #include <stdio.h>
-#include "../minijson_parser.h"
+#include "assertc.h"
 
-void open_file(char *file);
-void close_file();
+#include "minijson.h"
 
-int main(int argc, char **argv)
+module(minijson_parse)
 {
-    int opend_file = 0;
-    if (argc > 1)
+    suite("# invalid syntax")
     {
-        open_file(argv[1]);
-        opend_file = 1;
-    }
-    int ret = yyparse();
+        test("# empty buffer failes")
+        {
+            json_object_t *json = NULL;
 
-    if (opend_file)
+            json = json_parse("", 0);
+
+            expect_ptr(json) to be equal(NULL);
+        } end
+        test("# NULL buffer failes")
+        {
+            json_object_t *json = NULL;
+
+            json = json_parse(NULL, 0);
+
+            expect_ptr(json) to be equal(NULL);
+        } end
+        test("# unmatched curly braces '{' and '}'")
+        {
+            json_object_t *json = NULL;
+            json = json_parse("{", 1);
+            expect_ptr(json) to be equal(NULL);
+
+            json = json_parse("}", 1);
+            expect_ptr(json) to be equal(NULL);
+        } end
+        test("# unmatched square brackets '[' and ']'")
+        {
+            json_object_t *json = NULL;
+            json = json_parse("[", 1);
+            expect_ptr(json) to be equal(NULL);
+
+            json = json_parse("]", 1);
+            expect_ptr(json) to be equal(NULL);
+        } end
+    } end
+
+    suite("# Correct syntax")
     {
-        close_file();
-    }
+        test("# empty object '{}'")
+        {
+            json_object_t *json = NULL;
+            const string_t input = "{}";
+            json = json_parse(input, strlen(input));
 
-    return ret;
+            expect_ptr(json) to not be equal(NULL);
+        } end
+    } end
+}
+
+int main()
+{
+    register_module(minijson_parse);
+
+    run_all();
+    return 0;
 }
